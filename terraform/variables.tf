9812da6fdc7ccca96068c24087e884b998699182
Malik Hamdane <mfhamdane@hotmail.com>
2025-05-30 16:38:48 +0100

    Refactor variables.tf: aligned with dynamic subnet and SG resolution in main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,19 @@
-# Input variable definitions for Terraform project
-
-
-# VPC ID where all resources will be deployed
-variable "vpc_id" {
-  description = "The ID of the VPC where endpoints and resources will be created"
-  type        = string
+variable "ami_id" {
+    description =
+    "The AMI ID for the EC2 instance" type
+    = string
+} variable "instance_type" {
+    description =
+    "The type of EC2 instance to launch" type
+    = string
+    default =
+    "t3.micro"
+} variable "private_subnet_id_az1" {
+    description =
+    "Subnet ID for launching the EC2 instance in AZ1" type
+    = string
+} variable "vpc_id" {
+    description =
+    "VPC ID where the EC2 instance and security group will be deployed" type
+    = string
 }
