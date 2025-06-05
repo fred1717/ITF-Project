3acbcf596b33532f2d9bfa826ea46843f537bad2
Malik Hamdane <mfhamdane@hotmail.com>
2025-06-05 16:30:37 +0200

    update variables.tf with blank lines between blocks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,21 @@
 variable "ami_id" {
     description = "The AMI ID for the EC2 instance" 
     type = string
-} variable "instance_type" {
+} 
+
+variable "instance_type" {
     description = "The type of EC2 instance to launch" 
     type = string
     default = "t3.micro"
-} variable "private_subnet_id_az1" {
+} 
+
+variable "private_subnet_id_az1" {
     description = "Subnet ID for launching the EC2 instance in AZ1" 
     type = string
-} variable "vpc_id" {
+} 
+
+variable "vpc_id" {
     description = "VPC ID where the EC2 instance and security group will be deployed" 
     type = string
 }
+
